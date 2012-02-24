a8adc371427fd132776964e15cba815058196b44
Kevin Jiang <kevin.jiang@lexisnexis.com>
2012-02-24 15:44:43 +0800

    optimizer

--- cvs/getcodelist.sh
+++ cvs/getcodelist.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-rm -rf alpha/
+# --------------------------------------------------
+# 批量获取cvs模块里的文件
+# Author: Kevin
+# Email : kittymiky@gmail.com
+# 
+# command line format:
+#   ./getcodelist.sh 
+#
+# notice:
+#   首先需要更改module_name变量,改成自己的模块
+#   然后要登录到cvs, cvs -d $CVSROOT login
+#   然后运行此脚本即可
+# --------------------------------------------------
 
-cvs co -r "1.22" "alpha/law/search_form_case_plus.php"
-cvs co -r "1.6" "alpha/law/search_form_case_plus_en.php"
-cvs co -r "1.11" "alpha/law/search_form_expert.php"
-cvs co -r "1.11" "alpha/law/search_form_foreignlaw.php"
-cvs co -r "1.12" "alpha/law/template/search_form_expert.html"
-cvs co -r "1.12" "alpha/law/template/search_form_newlaw.html"
-cvs co -r "1.7" "alpha/law/template/search_form_newsletter.html"
-cvs co -r "1.19" "alpha/law/template/search_form_book.html"
+module_name="alpha"
 
-find alpha/ -name 'CVS' -type d | while read f; do rm -rf $f; done
+rm -rf $module_name/
+
+cvs co -r "1.22" "$module_name/law/search_form_case_plus.php"
+
+find $module_name/ -name 'CVS' -type d | while read f; do rm -rf $f; done
