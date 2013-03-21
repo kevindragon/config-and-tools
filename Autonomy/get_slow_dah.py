10d2f5cde1b451330dd2cf8f14c910b4368220e2
Kevin Jiang <kevin.jiang@lexisnexis.com>
2013-03-21 18:25:53 +0800

    add autonomy log analysis script to get slow query command

---
+++ Autonomy/get_slow_dah.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__doc__ = """获取IDOL日志里面比较慢的请求"""
+
+import sys
+import re
+
+filename = sys.argv[1]
+
+#read file content
+logs = [ x.strip() for x in open(filename, "r").readlines()]
+
+p = re.compile("\[(\d)\]\s*Engine \d returned in ([0-9.]+) ([^.]+)")
+x = 0
+slow_logs = []
+for n, line in enumerate(logs):
+    matched = p.findall(line)
+    if len(matched):
+        if "seconds"==matched[0][2] and float(matched[0][1]) > 1:
+            tmp_slow = matched[0][1]
+            point = n-1
+            while point > 0:
+                if re.search("\d\d \["+matched[0][0]+"\] action=", logs[point], re.IGNORECASE):
+                    tmp_slow = "%s; %s" % (tmp_slow, logs[point])
+                    break
+                point -= 1
+            slow_logs.append(tmp_slow)
+
+
+print "\n".join(slow_logs)
