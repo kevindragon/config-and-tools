b9ae31378f2a30c2ce43fc744568d96aa642cf62
Kevin Jiang <kittymiky@gmail.com>
2013-03-29 17:35:50 +0800

    完善NFS监控脚本

--- Autonomy/nfscheck.sh
+++ Autonomy/nfscheck.sh
@@ -9,33 +9,49 @@
 
 log_file="/var/log/nfscheck.log"
 prog="nfscheck.sh"
-source="10.123.4.22:/home/www/word"
-target=/root/word
+src1="10.123.4.22:/home/www/word"
+src2="10.123.4.22:/home/www/newsletters"
+src3="10.123.4.22:/home/www/ipatt"
+target1="/root/word"
+target2="/root/newsletters"
+target3="/root/ipatt"
 
-if [ `ps -C $prog --no-header | wc -l` -ne 0 ];then
-    echo "$prog is running. exit" >> $log_file
-    exit
-fi 
+FAILED_EXIT=2
+failed_count=0
 
-s=`ls $target 2>&1 | grep "Stale NFS file handle" | wc -l`
-
-if [[ $s -eq 1 ]];then
-    echo "`date`" >> $log_file
-    echo "Stale NFS file handle" >> $log_file
-    echo -n "umount $target" >> $log_file
-    umount -f $target
-    if [ $? -eq 0 ];then
-        echo "umount success" >> $log_file
-        echo "re-mount $target" >> $log_file
-        mount -t nfs $source $target
-        if [ $? -ne 0 ];then
-            echo "re-mount failed." >> $log_file
+while :
+do
+    s=`ls $target1 2>&1 | grep "Stale NFS file handle" | wc -l`
+    if [[ $s -eq 1 ]];then
+        echo "`date`" >> $log_file
+        echo "Stale NFS file handle" >> $log_file
+        echo "umount $target1 $target2 $target3" >> $log_file
+        umount -f $target1 $target2 $target3
+        if [ $? -eq 0 ];then
+            echo "umount success" >> $log_file
+            echo "re-mount $target1 $target2 $target3" >> $log_file
+            mount -t nfs $src1 $target1
+            mount -t nfs $src2 $target2
+            mount -t nfs $src3 $target3
+            if [ $? -ne 0 ];then
+                echo "re-mount failed." >> $log_file
+                failed_count=$((failed_count+1))
+            else
+                failed_count=0
+                echo "re-mount success" >> $log_file
+            fi
         else
-            echo "re-mount success" >> $log_file
+            echo "umount failed." >> $log_file
+            echo "nfs can not connect on $HOSTNAME" | mail -s "stale nfs" kevin.jiang@lexisnexis.com
+            echo "nfs can not connect on $HOSTNAME" | mail -s "stale nfs" 13918543465@139.com
         fi
-    else
-        echo "umount failed." >> $log_file
-        echo "nfs can not connect on $HOSTNAME" | mail -s "stale nfs" kevin.jiang@lexisnexis.com
-        echo "nfs can not connect on $HOSTNAME" | mail -s "stale nfs" 13918543465@139.com
     fi
-fi
+    
+    if [ $failed_count -gt $FAILED_EXIT ];then
+        echo "nfs can not connect on $HOSTNAME. tried $FAILED_EXIT times" | mail -s "stale nfs" kevin.jiang@lexisnexis.com
+        echo "nfs can not connect on $HOSTNAME. tried $FAILED_EXIT times" | mail -s "stale nfs" 13918543465@139.com
+    fi
+
+    sleep 600
+done
+
